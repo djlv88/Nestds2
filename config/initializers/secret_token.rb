83821fb0f6fc8a09eb1e3b0fff3998cbe6ba570f
djlv88 <lutherrmoore@gmail.com>
2013-01-30 21:59:26 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nestds2::Application.config.secret_token = 'e2907386cf94bc26a6a744a0b05c6c190fee700bb158cf5f87db050eee4fcc81b7abeee1ed745e61f220f9a1570232f300b20f9617c703155a28ef231de734a3'
